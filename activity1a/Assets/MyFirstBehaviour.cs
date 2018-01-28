b9e583ec48b59888c70ce39a89da7a0f1a5c9d6f
Kaaoks <pip_s@hotmail.fr>
2018-01-27 21:23:36 -0800

    Activity1a

---
+++ activity1a/Assets/MyFirstBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstBehaviour : MonoBehaviour {
+    public int repeatNo;
+    private int cpt = 0;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        Debug.Log(transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cpt++;
+        while (repeatNo > 0)
+        {
+            if (cpt == 50)
+            {
+                cpt = 0;
+                print(repeatNo);
+                repeatNo--;
+            }
+        }
+	}
+}
