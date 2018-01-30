f569c6ea11a10a4704df8d81f61c15eb8e87edc6
Pierre Groell <groe8549@csumb.ad.csumb.edu>
2018-01-30 10:57:46 -0800

    pong

---
+++ Pong/Assets/BallScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(100, 10, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Pong/Assets/ControlRPaddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlRPaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow) && transform.position.y < 3.0f)
+        {
+            transform.position += new Vector3(0, 0.1f , 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -3.0f)
+        {
+            transform.position += new Vector3(0, -0.1f, 0);
+        }
+	}
+}
